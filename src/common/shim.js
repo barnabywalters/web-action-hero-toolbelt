6fa1f2b2157b7408a661e15c8966b3105f60d287
Barnaby Walters <barnaby@waterpigs.co.uk>
2013-03-01 18:53:03 +0000

    Improved WebActionHero definition, improved private zepto gathering routing, removed <h1> in config

--- src/common/shim.js
+++ src/common/shim.js
@@ -3,7 +3,21 @@
 // @include *
 // ==/UserScript==
 
-var WebActionHero = {};
+var WebActionHero = (function () {
+    // Private
+    var $; // Zepto
+    
+    // Public
+    return {
+        init: function() {
+            
+        },
+        
+        setZepto: function(zepto) {
+            $ = zepto;
+        }
+    };
+}());
 
 // Include Zepto under own namespace to prevent meddling with site scripts
 var oldZepto = window.Zepto;
@@ -11,19 +25,9 @@ var oldDollar = window.$;
 
 docment.write('<script src="//cdnjs.cloudflare.com/ajax/libs/zepto/1.0rc1/zepto.min.js"></script>');
 
-WebActionHero.$ = Zepto;
+WebActionHero.setZepto(Zepto);
 
 window.Zepto = oldZepto;
 window.$ = oldDollar;
 
-// Do the main stuff after a short time
-document.setTimeout(WebActionHero.main, 700);
-
-// TODO: Implement this
-WebActionHero.main = function () {
-    // Load Config
-    
-    // Replace markup with replacements with <action> elements
-    
-    // Add UI to all <action> elements
-};
+WebActionHero.init();
