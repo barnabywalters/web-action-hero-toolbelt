25bf3d5e63466b0fb9c1587a740f24f8881d3724
Barnaby Walters <barnaby@waterpigs.co.uk>
2013-03-01 20:47:47 +0000

    Added a load of verb utility functions and logic boilerplate

--- output/chrome/includes/content_init.js
+++
@@ -1,5 +0,0 @@
-﻿/*
-Built using Kango - Cross-browser extension framework
-http://kangoextensions.com/
-*/
-window.addEventListener("DOMContentLoaded",function(){kango._init("document-end")},!1);kango._init("document-start");
--- src/common/shim.js
+++ src/common/shim.js
@@ -6,11 +6,66 @@
 var WebActionHero = (function () {
     // Private
     var $; // Zepto
+    var verbs;
+    var webActionTemplate = [
+        '<div style="background:#222; display:inline-block; padding-right: 0.33em; border:#555 solid 1px; border-radius: 0.3em;" class="web-action-hero-toolbelt-button">',
+            '<button style="padding:0.5em">Verb This</button>',
+            '<select style="width: 1.5em;"></select>',
+        '</div>'
+    ].join();
+    
+    // Returns the verb object or null for verb name
+    function verb(name) {
+        for (v in verbs) {
+            if (v.name === name)
+                return v;
+        }
+        
+        return null;
+    }
+    
+    // Checks to see if there are valid (name and url nonempty) service(s)
+    // defined for a given verb
+    function verbDefined(name) {
+        var verb = verb(name);
+        
+        if (verb === null)
+            return false;
+        
+        if (verb.services.length === 0)
+            return false;
+        
+        if (verb.services.every(function (service) {
+            return service.name === '' || service.url === ''
+                || service.name === null || service.url === null;
+        }))
+            return false;
+        
+        return true;
+    }
+    
+    function main(config) {
+        verbs = config.verbs;
+        replace();
+        activateWebActions();
+    }
+    
+    // Container function which calls functions which do UI/button customisation,
+    // replacing them with web action elements
+    function replace() {
+        // TODO
+    }
+    
+    // Goes through all <action> elements, replaces their fallback content with
+    // a verb UI
+    function activateWebActions() {
+        // TODO
+    }
     
     // Public
     return {
         init: function() {
-            
+            kango.invokeAsync('kango.storage.getItem', 'config', main);
         },
         
         setZepto: function(zepto) {
